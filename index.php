eba9a77464683082ce33599c490e07bf465c2133
jsjlabsdev <73208494+jsjlabsdev@users.noreply.github.com>
2021-05-25 15:41:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="css/main.css"></head>
+    <link rel="stylesheet" href="main.css"></head>
 <body>
     
     <div class="container">
